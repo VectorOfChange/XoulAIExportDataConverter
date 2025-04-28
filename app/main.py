c24aa422c389c863522550f0ec4db34c78bc2718
VectorOfChange <104664424+VectorOfChange@users.noreply.github.com>
2025-04-28 15:56:19 +0200

    Troubleshoot github actions
    Signed-off-by: VectorOfChange <104664424+VectorOfChange@users.noreply.github.com>

--- app/main.py
+++ app/main.py
@@ -564,7 +564,7 @@ st.markdown("""
                 * GUI: Fix and enhance progress bar
                 * FIX: Only include bugs that impact data in the documents
             * Version ```0.0.5```:
-                * 
+                * DEV: Add multi-host platform management
             """)
 
 st.subheader("Page suddenly scrolled to the bottom? It's a bug. Sorry about that! Scroll up ⬆️ to find your files again.", divider=False)
